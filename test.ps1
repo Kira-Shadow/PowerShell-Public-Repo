00a6bab9bc8bb2b67f1ccbb7afa97bdfb19e8f44
Kira-Shadow <Kira@felinefemale.co.uk>
2021-08-21 14:30:00 +0100

    Just messing about

--- test.ps1
+++ test.ps1
@@ -1,1 +1,4 @@
-get-service|ogv
+# Please note, this isn't an example of my powershell skills, if it was I would go an diliver pizzas for a living (It is actualy a fun job)
+# I'm just playing with Git
+
+Get-Service|Out-GridView -Title "Some title goes here!!"
