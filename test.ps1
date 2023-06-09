7f1ed77a03e78430b465b8b2b806fa5f7af603cd
Kira <Kira@felinefemale.co.uk>
2023-06-09 09:12:04 +0100

    clean

--- test.ps1
+++
@@ -1,4 +0,0 @@
-# Please note, this isn't an example of my powershell skills, if it was I would go an deliver pizzas for a living (It is actualy a fun job)
-# I'm just playing with Git
-
-Get-Service|Out-GridView -Title "Some title goes here!!"
